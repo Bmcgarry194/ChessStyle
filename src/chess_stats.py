f54fde105d166a4907c9271326e37f1c9a494c75
Brian McGarry <bmcgarry816@gmail.com>
2018-08-20 16:14:05 -0700

    added piece count graphs

--- src/chess_stats.py
+++ src/chess_stats.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import json
-import random
-import datetime
 import requests
 import numpy as np
-import re
+import chess
+import chess.pgn
+from io import StringIO
 
 def get_monthly_archives(username):
     '''Returns a list of months in which the user played a game'''
+    
     try:
         response = requests.get(f'https://api.chess.com/pub/player/{username}/games/archives')
-        months_played = json.loads(response.content.decode('utf-8'))['archives']
-        return [month[-7:].split('/') for month in months_played]
+        return json.loads(response.content.decode('utf-8'))['archives'] 
     except:
         return []
         
@@ -22,9 +22,7 @@ def get_player_games(username):
     games = []
     if months:
         for month in months:
-            year = month[0]
-            month_ = month[1]
-            response = requests.get(f'https://api.chess.com/pub/player/{username}/games/{year}/{month_}')
+            response = requests.get(month)
             for game in json.loads(response.content.decode('utf-8'))['games']:
                 games.append(game)
     return games
@@ -44,8 +42,11 @@ def game_stats_df(username):
     rated = []
     time_class = []
     rules = []
-    game_length = []
     player_color = []
+    pieces_diff = []
+    pieces_total = []
+    pieces_black = []
+    pieces_white = []
 
     for i, game in enumerate(game_list):
         
@@ -55,12 +56,10 @@ def game_stats_df(username):
             pgn_data = game['pgn'].split('\n')
             pgn.append(pgn_data[-1])
             eco.append(pgn_data[7].split('"')[-2])
-            game_length.append(int(re.split("(\d+)\.+", game['pgn'])[-2]))
 
         else:
             pgn.append(None)
             eco.append(None)
-            game_length.append(None)
             
         if game['black']['username'].lower() == username.lower():
             player_rating.append(game['black']['rating'])
@@ -90,8 +89,7 @@ def game_stats_df(username):
                        'time_class': time_class,
                        'rules': rules,
                        'pgn': pgn,
-                       'eco': eco,
-                       'game_length':game_length})
+                       'eco': eco,})
     
 #Create three columns in the data frame indicating a win, draw, or loss with a True or False
     df['win'] = df['player_result'] == 'win'
@@ -113,3 +111,41 @@ def eco_labels(filepath):
     with open(filepath) as infile:
         eco_names = json.load(infile)
     return eco_names
+
+def piece_count(pgn_string):
+    '''Returns piece value balance for every move of the game.
+    each piece has a value: pawns(p) = 1, bishops(b)/knights(n) = 3, rooks(r) = 5, queen(q) = 9,
+    For every move the difference in piece value is calculate by white piece value - black piece value.
+    So a positive number means you are ahead in material and and negitive number indicates you are down in material.
+    
+    Arg: 
+        pgn_string: Takes a string of a chess game in pgn notation
+    Returns:
+        A tuple of lists of numbers corresponding to piece value at each move: (diff, total, black, white)
+            diff = Difference in piece value, white adv is a positive int and black adv is a negitive int
+            total = Total piece value for both players
+            black = black piece value
+            white = white piece value'''
+    
+    if not pgn_string:
+        return []
+    
+    pgn = StringIO(pgn_string)
+    game = chess.pgn.read_game(pgn)
+    board = game.board()
+
+    diff = []
+    total = []
+    black = []
+    white = []
+    for move in game.main_line():
+        board.push(move)
+        board_state = board.fen().split()[0]
+        black_score = board_state.count('p') + board_state.count('b')*3 + board_state.count('r')*5 + board_state.count('n')*3 +                         board_state.count('q')*9
+        white_score = board_state.count('P') + board_state.count('R')*5 + board_state.count('B')*3 + board_state.count('N')*3 +                         board_state.count('Q')*9
+        diff.append(white_score - black_score)
+        total.append(black_score + white_score)
+        black.append(black_score)
+        white.append(white_score)
+    
+    return (diff, total, black, white)
